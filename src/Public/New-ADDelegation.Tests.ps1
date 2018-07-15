6775ec60b1a31712025d247b4ef04bd3ff771440
Mathias R. Jessen <mathias@mrjessen.com>
2018-07-15 23:03:59 +0200

    Add New-ADDelegation

---
+++ src/Public/New-ADDelegation.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "New-ADDelegation" {
+    It "does something useful" {
+        Delegate -Task 'Reset Passwords' -To 'Someone'
+    }
+}
