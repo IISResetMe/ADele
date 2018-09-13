20517cd71b2caefc6114ac124dab7bc02e2ae3c4
Mathias R. Jessen <IISResetMe@users.noreply.github.com>
2018-09-13 14:08:40 +0200

    Add attribute lookup by GUID
    Removed the ShowGuid option to avoid unnecessary parameter set complexity

--- src/Public/Get-ADSchemaAttribute.ps1
+++ src/Public/Get-ADSchemaAttribute.ps1
@@ -1,10 +1,13 @@
 function Get-ADSchemaAttribute {
-    [CmdletBinding(DefaultParameterSetName='__default')]
+    [CmdletBinding(DefaultParameterSetName='ByName')]
     param(
-        [Parameter(Mandatory=$true,Position=0)]
+        [Parameter(Mandatory=$true,Position=0,ParameterSetName='ByName')]
         [Alias('lDAPDisplayName')]
         [ValidateNotNullOrEmpty()]
         [string]$Name,
+
+        [Parameter(Mandatory=$true,Position=0,ParameterSetName='ByGuid')]
+        [guid]$Guid,
 
         [Parameter(Mandatory=$false)]
         [ValidateNotNullOrEmpty()]
@@ -22,10 +25,7 @@ function Get-ADSchemaAttribute {
         [ValidateNotNullOrEmpty()]
         [pscredential]$Credential,
 
-        [Parameter(Mandatory=$true,ParameterSetName='GuidOnly')]
-        [switch]$ShowGuid,
-
-        [Parameter(Mandatory=$true,ParameterSetName='SyntaxOnly')]
+        [Parameter(Mandatory=$false)]
         [switch]$Syntax
     )
 
@@ -34,7 +34,7 @@ function Get-ADSchemaAttribute {
 
     # Remove parameters specific to this function from $PSBoundParameters
     # Allows for easy param reuse with ActiveDirectory cmdlets
-    'Name','Syntax','ShowGuid' |ForEach-Object {
+    'Name','Syntax','Guid' |ForEach-Object {
         $PSBoundParameters.Remove($_)
      } |Out-Null
 
@@ -48,10 +48,10 @@ function Get-ADSchemaAttribute {
         'attributeSyntax'
         'schemaIDGUID'
         'oMSyntax'
-        )
+    )
 
-    # User wants whole definition, pull additional attribute values
-    if($PSCmdlet.ParameterSetName -eq '__default'){
+    # User wants whole definition, not just syntax, pull additional attribute values
+    if(-not $Syntax){
         $PSBoundParameters['Properties'] += @(
             'attributeID'
             'attributeSyntax'
@@ -62,44 +62,48 @@ function Get-ADSchemaAttribute {
         )
     }
 
-    $attrSchema = Get-ADObject -Filter "lDAPDisplayName -eq '$Name' -and objectClass -eq 'attributeSchema'" @PSBoundParameters
-
-    switch -Exact ($PSCmdlet.ParameterSetName){
-        'GuidOnly' {
-            return $attrSchema.schemaIDGUID -as [guid]
+    $attrSchema = switch -Exact ($PSCmdlet.ParameterSetName){
+        'ByName' {
+            Get-ADObject -Filter "lDAPDisplayName -eq '$Name' -and objectClass -eq 'attributeSchema'" @PSBoundParameters
         }
-        'SyntaxOnly' {
-
-            $SyntaxTable = @{
-                '2.5.5.8:1'    = 'Boolean'
-                '2.5.5.9:10'   = 'Enumeration'
-                '2.5.5.9:2'    = 'Integer'
-                '2.5.5.16:65'  = 'LargeInteger'
-                '2.5.5.14:127' = 'Object(DN-String)'
-                '2.5.5.7:127'  = 'Object(DN-Binary)'
-                '2.5.5.1:127'  = 'Object(DS-DN)'
-                '2.5.5.13:127' = 'Object(Presentation-Address)'
-                '2.5.5.10:127' = 'Object(Replica-Link)'
-                '2.5.5.3:27'   = 'String(Case)'
-                '2.5.5.5:22'   = 'String(IA5)'
-                '2.5.5.15:66'  = 'String(NT-Sec-Desc)'
-                '2.5.5.6:18'   = 'String(Numeric)'
-                '2.5.5.2:6'    = 'String(Object-Identifier)'
-                '2.5.5.10:4'   = 'String(Octet)'
-                '2.5.5.5:19'   = 'String(Printable)'
-                '2.5.5.17:4'   = 'String(Sid)'
-                '2.5.5.4:20'   = 'String(Teletex)'
-                '2.5.5.12:64'  = 'String(Unicode)'
-                '2.5.5.11:23'  = 'String(UTC-Time)'
-                '2.5.5.11:24'  = 'String(Generalized-Time)'
-            }
-            return [PSCustomObject]@{
-                Name   = $SyntaxTable[@($attrSchema.attributeSyntax;$attrSchema.oMSyntax)-join':']
-                OID    = $attrSchema.attributeSyntax
-                Syntax = $attrSchema.oMSyntax
-            }
+        'ByGuid' {
+            Get-ADObject -Filter "schemaIDGUID -eq '$Guid' -and objectClass -eq 'attributeSchema'" @PSBoundParameters
         }
     }
 
-    return $attrSchema
+    # Resolve syntax description
+    $SyntaxTable = @{
+        '2.5.5.8:1'    = 'Boolean'
+        '2.5.5.9:10'   = 'Enumeration'
+        '2.5.5.9:2'    = 'Integer'
+        '2.5.5.16:65'  = 'LargeInteger'
+        '2.5.5.14:127' = 'Object(DN-String)'
+        '2.5.5.7:127'  = 'Object(DN-Binary)'
+        '2.5.5.1:127'  = 'Object(DS-DN)'
+        '2.5.5.13:127' = 'Object(Presentation-Address)'
+        '2.5.5.10:127' = 'Object(Replica-Link)'
+        '2.5.5.3:27'   = 'String(Case)'
+        '2.5.5.5:22'   = 'String(IA5)'
+        '2.5.5.15:66'  = 'String(NT-Sec-Desc)'
+        '2.5.5.6:18'   = 'String(Numeric)'
+        '2.5.5.2:6'    = 'String(Object-Identifier)'
+        '2.5.5.10:4'   = 'String(Octet)'
+        '2.5.5.5:19'   = 'String(Printable)'
+        '2.5.5.17:4'   = 'String(Sid)'
+        '2.5.5.4:20'   = 'String(Teletex)'
+        '2.5.5.12:64'  = 'String(Unicode)'
+        '2.5.5.11:23'  = 'String(UTC-Time)'
+        '2.5.5.11:24'  = 'String(Generalized-Time)'
+    }
+    $attrSyntax = [PSCustomObject]@{
+        Name   = $SyntaxTable[@($attrSchema.attributeSyntax;$attrSchema.oMSyntax)-join':']
+        OID    = $attrSchema.attributeSyntax
+        Syntax = $attrSchema.oMSyntax
+    }
+
+    if($Syntax){
+        return $attrSyntax
+    }
+
+    return $attrSchema |Add-Member -MemberType NoteProperty -Name Syntax -Value $attrSyntax -PassThru
 }
